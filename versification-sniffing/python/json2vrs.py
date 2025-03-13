32785a8e70f4cd399d87fb50edfaa4689f5be95f
Robertson Brinker <robertsonbrinker@gmail.com>
2025-03-13 08:51:00 -0400

    using arrays in json2vrs

--- versification-sniffing/python/json2vrs.py
+++ versification-sniffing/python/json2vrs.py
@@ -44,8 +44,7 @@ for key in base["partialVerses"].keys():
 	print("*"+",".join([key]+base["partialVerses"][key]))
 
 print("# Verse Mappings")
-for mapping in base["mappedVerses"]:
-    for key, value in mapping.items():
+for key in base["verseMappings"].keys():
+    value_array = base["verseMappings"][key]
+    for value in value_array:
         print(key + " = " + value)
-        
-#update this to accept new json format and transform to vrs properly
